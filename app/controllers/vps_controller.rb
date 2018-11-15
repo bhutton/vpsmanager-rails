7830b1c1bbe71d7ded7b3b523b647839270d6da1
bhutton <ben@benhutton.com.au>
2018-11-15 21:49:49 +0800

    Initialise repo and created homepage for vps.

---
+++ app/controllers/vps_controller.rb
@@ -0,0 +1,5 @@
+class VpsController < ApplicationController
+  def index
+
+  end
+end
---
+++ test/controllers/vps_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+describe 'My behaviour', :type => :request do
+
+  it 'should do something' do
+    get vps_url
+  end
+end
