2c9943a3ffd25a8effcf89d3b5dbdd7b90d528ec
bhutton <mefmnoVES9M6cAMa>
2018-12-10 10:30:03 +0800

    Resolved delete test on instance_controller_spec.

--- spec/controller/instances_controller_spec.rb
+++ spec/controller/instances_controller_spec.rb
@@ -1,14 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe InstancesController, :type => :controller do
-  # describe "INDEX" do
-  #   it 'should display main page' do
-  #     expect{index}.to contain_exactly()
-  #   end
-  # end
   describe "DELETE #destroy" do
-    it 'should do something' do
+    it 'should delete item' do
       instance = Instance.new(id: 1, name: "test")
+      instance.save
 
       expect{delete :destroy, params: { id: instance.id }}.to change(Instance, :count).by(-1)
     end
